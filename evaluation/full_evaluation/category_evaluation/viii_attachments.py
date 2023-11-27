95e98a569eb48f3e6b4c0e2677af7f6662e52323
jgroschwitz <jonasg@coli.uni-saarland.de>
2023-11-27 14:54:29 +0100

    all categories have evaluation function now

--- evaluation/full_evaluation/category_evaluation/viii_attachments.py
+++ evaluation/full_evaluation/category_evaluation/viii_attachments.py
@@ -5,12 +5,13 @@ from evaluation.pp_attachment import get_pp_attachment_success_counters
 
 from evaluation.testset.ne_types import get_ne_type_successes_and_sample_size
 
+
 class Attachments(CategoryEvaluation):
 
     def _run_all_evaluations(self):
         self.set_dataset_name("PP attachment")
-        prereqs, unlabeled, recalled, sample_size = get_pp_attachment_success_counters(self.root_dir+"/corpus/",
-                                                                               self.root_dir+f"/{self.parser_name}-output/")
+        prereqs, unlabeled, recalled, sample_size = get_pp_attachment_success_counters(self.root_dir + "/corpus/",
+                                                                                       self.root_dir + f"/{self.parser_name}-output/")
         self.make_and_append_results_row("Edge recall", EVAL_TYPE_SUCCESS_RATE, [recalled, sample_size])
         self.make_and_append_results_row("Unlabeled edge recall", EVAL_TYPE_SUCCESS_RATE, [unlabeled, sample_size])
         self.make_and_append_results_row("Prerequisites", EVAL_TYPE_SUCCESS_RATE, [prereqs, sample_size])
@@ -31,4 +32,28 @@ class Attachments(CategoryEvaluation):
         self.set_dataset_name("Unaccusatives")
         self.make_results_columns_for_edge_recall("unaccusatives2_filtered.tsv", use_sense=True)
 
+    def compute_pp_results(self, gold_amrs, predicted_amrs):
+        prereqs, unlabeled, recalled, sample_size = get_pp_attachment_success_counters(gold_amrs, predicted_amrs)
+        return [self.make_results_row("Edge recall", EVAL_TYPE_SUCCESS_RATE, [recalled, sample_size]),
+                self.make_results_row("Unlabeled edge recall", EVAL_TYPE_SUCCESS_RATE, [unlabeled, sample_size]),
+                self.make_results_row("Prerequisites", EVAL_TYPE_SUCCESS_RATE, [prereqs, sample_size])]
 
+    def compute_unbounded_results(self, gold_amrs, predicted_amrs):
+        return self.make_results_columns_for_edge_recall_from_graphs("unbounded_dependencies.tsv",
+                                                                     gold_amrs,
+                                                                     predicted_amrs,
+                                                                     use_sense=False,
+                                                                     source_column=2, edge_column=3, target_column=4,
+                                                                     first_row_is_header=True)
+
+    def compute_passive_results(self, gold_amrs, predicted_amrs):
+        return self.make_results_columns_for_edge_recall_from_graphs("passives_filtered.tsv",
+                                                                     gold_amrs,
+                                                                     predicted_amrs,
+                                                                     use_sense=True)
+
+    def compute_unaccusative_results(self, gold_amrs, predicted_amrs):
+        return self.make_results_columns_for_edge_recall_from_graphs("unaccusatives2_filtered.tsv",
+                                                                     gold_amrs,
+                                                                     predicted_amrs,
+                                                                     use_sense=True)
